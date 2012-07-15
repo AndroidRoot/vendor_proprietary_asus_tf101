c678fa53b4fd90eb43d5d039ffb8d9572adf4cea
Jens Andersen <jens.andersen@gmail.com>
2012-07-15 12:22:36 +0200

    Disable audio hal

--- tf101-vendor-blobs.mk
+++ tf101-vendor-blobs.mk
@@ -48,8 +48,6 @@ PRODUCT_COPY_FILES += \
     vendor/asus/tf101/proprietary/lib/egl/libEGL_perfhud.so:system/lib/egl/libEGL_perfhud.so \
     vendor/asus/tf101/proprietary/lib/egl/libGLESv1_CM_perfhud.so:system/lib/egl/libGLESv1_CM_perfhud.so \
     vendor/asus/tf101/proprietary/lib/egl/libGLESv2_perfhud.so:system/lib/egl/libGLESv2_perfhud.so \
-    vendor/asus/tf101/proprietary/lib/hw/audio.primary.tegra.so:system/lib/hw/audio.primary.tegra.so \
-    vendor/asus/tf101/proprietary/lib/hw/audio_policy.tegra.so:system/lib/hw/audio_policy.tegra.so \
     vendor/asus/tf101/proprietary/lib/hw/camera.tegra.so:system/lib/hw/camera.tegra.so \
     vendor/asus/tf101/proprietary/lib/hw/gps.ventana.so:system/lib/hw/gps.ventana.so \
     vendor/asus/tf101/proprietary/lib/hw/gralloc.tegra.so:system/lib/hw/gralloc.tegra.so \
